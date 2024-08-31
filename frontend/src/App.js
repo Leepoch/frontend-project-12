285b9b9aeeade600225de916627e3bbe0912a6fc
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-08-31 15:51:34 +0300

    add login and notFound

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './login.jsx';
 
-const App = () => <h1>Hexlet Chat!</h1>;
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
